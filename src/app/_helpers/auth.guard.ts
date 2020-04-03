f3a5a9f8921d09665e2fc4496e18c996835333e5
thiago <foalthik@gmail.com>
2020-04-03 15:17:25 -0300

    sidebar

--- src/app/_helpers/auth.guard.ts
+++ src/app/_helpers/auth.guard.ts
@@ -1,25 +1,16 @@
 ﻿import { Injectable } from '@angular/core'
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router'
 
-import { AuthenticationService, UserService } from '@app/_services'
+import { UserService } from '@app/_services'
 import { User } from '@app/_models'
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
   user: User
 
-  constructor(
-    private router: Router,
-    private authenticationService: AuthenticationService,
-    private userService: UserService
-  ) {}
+  constructor(private router: Router, private userService: UserService) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const currentUser = this.authenticationService.currentUserValue
-
-    // // not logged in so redirect to login page with the return url
-    // this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } })
-
     this.userService.getUser().subscribe((user) => {
       console.log('USER ON CanActive', user)
       if (user && user.client_id) {
@@ -35,6 +26,9 @@ export class AuthGuard implements CanActivate {
         return true
       }
     })
-    return true
+
+    // // not logged in so redirect to login page with the return url
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } })
+    return false
   }
 }
