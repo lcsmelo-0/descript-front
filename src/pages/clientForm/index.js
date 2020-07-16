76ad6c2a29f85d68ff6a912465892e92c038146a
Thiago Pereira <foalthik@gmail.com>
2020-07-16 09:22:03 -0300

    save files

--- src/pages/clientForm/index.js
+++ src/pages/clientForm/index.js
@@ -1,57 +1,48 @@
-import React, { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { Creators as ImageActions } from "../../store/ducks/image"
+import { Creators as ImageActions } from "../../store/ducks/image";
 
-import { useFormFields } from "../../utils/hooks"
+import { useFormFields } from "../../utils/hooks";
 
-import Spinner from "../../components/icons/Spinner"
+import Spinner from "../../components/icons/Spinner";
 
-import {
-  Container,
-  FormGroup,
-  Label,
-  Input,
-  Form
-
-} from "reactstrap"
+import { Container, FormGroup, Label, Input, Form } from "reactstrap";
 
 const ClientForm = props => {
   const [fields, handleFieldsChange] = useFormFields({
     name: "",
     file: "",
     description: ""
-  })
-  const dispatch = useDispatch()
+  });
+  const dispatch = useDispatch();
 
-
-
-  const [image, setImage] = useState({ preview: '' })
-  const { loading } = useSelector(state => state.image)
+  const [image, setImage] = useState({ preview: "" });
+  const { loading } = useSelector(state => state.image);
 
   useEffect(() => {
-    dispatch(ImageActions.getImagesRequest())
-  }, [])
+    dispatch(ImageActions.getImagesRequest());
+  }, []);
 
   const handleImage = event => {
-    const reader = new FileReader()
+    const reader = new FileReader();
     if (event.target.files && event.target.files.length) {
-      const [file] = event.target.files
-      reader.readAsDataURL(file)
+      const [file] = event.target.files;
+      reader.readAsDataURL(file);
       reader.onload = () => {
-        fields.file = reader.result
+        fields.file = reader.result;
         setImage({
           preview: reader.result
-        })
-      }
+        });
+      };
     }
-  }
+  };
 
   const handleSubmit = event => {
-    event.preventDefault()
-    dispatch(ImageActions.setImageRequest(fields))
-    return props.history.goBack()
-  }
+    event.preventDefault();
+
+    dispatch(ImageActions.setImageRequest(fields));
+  };
 
   return (
     <>
@@ -60,49 +51,75 @@ const ClientForm = props => {
           <Spinner />
         </div>
       ) : (
-          <>
-            {console.log(fields)}
-            <Container>
-
-              <section className="slice py-5 py-lg-7">
-                <div className="container">
-                  <div className="row row-grid align-items-center">
-                    <Form class="md-form d-flex justify-content-center" onSubmit={handleSubmit}>
-                      <div class="file-field">
-                        <div class="z-depth-1-half mb-4">
-                          {image.preview ? (
-                            <img src={image.preview} alt="dummy" height="400" />
-                          ) : (
-                              <img src="https://mdbootstrap.com/img/Photos/Others/placeholder.jpg" class="img-fluid" alt="example placeholder" />
-                            )}
-                        </div>
-                        <div class="d-flex justify-content-center">
-                          <div class="btn btn-mdb-color btn-rounded float-left">
-                            <span>Choose file</span>
-                            <input type="file" onChange={handleImage} />
-                          </div>
-                        </div>
-                        <FormGroup>
-                          <Label for="exampleFormControlTextarea1">Nome da imagem</Label>
-                          <Input type="text" name="name" onChange={handleFieldsChange} id="name" />
-                        </FormGroup>
-                        <FormGroup>
-                          <Label for="exampleFormControlTextarea1">Descrição da imagem</Label>
-                          <textarea class="form-control" name="description" onChange={handleFieldsChange} id="description" rows="3"></textarea>
-                        </FormGroup>
+        <>
+          {console.log(fields)}
+          <Container>
+            <section className="slice py-5 py-lg-7">
+              <div className="container">
+                <div className="row row-grid align-items-center">
+                  <Form
+                    class="md-form d-flex justify-content-center"
+                    onSubmit={handleSubmit}
+                  >
+                    <div class="file-field">
+                      <div class="z-depth-1-half mb-4">
+                        {image.preview ? (
+                          <img src={image.preview} alt="dummy" height="400" />
+                        ) : (
+                          <img
+                            src="https://mdbootstrap.com/img/Photos/Others/placeholder.jpg"
+                            class="img-fluid"
+                            alt="example placeholder"
+                          />
+                        )}
                       </div>
-                      <div className="mt-4">
-                        <button type="submit" className="btn btn-block btn-primary">Entrar</button>
+                      <div class="d-flex justify-content-center">
+                        <div class="btn btn-mdb-color btn-rounded float-left">
+                          <span>Choose file</span>
+                          <input type="file" onChange={handleImage} />
+                        </div>
                       </div>
-                    </Form>
-                  </div>
+                      <FormGroup>
+                        <Label for="exampleFormControlTextarea1">
+                          Nome da imagem
+                        </Label>
+                        <Input
+                          type="text"
+                          name="name"
+                          onChange={handleFieldsChange}
+                          id="name"
+                        />
+                      </FormGroup>
+                      <FormGroup>
+                        <Label for="exampleFormControlTextarea1">
+                          Descrição da imagem
+                        </Label>
+                        <textarea
+                          class="form-control"
+                          name="description"
+                          onChange={handleFieldsChange}
+                          id="description"
+                          rows="3"
+                        ></textarea>
+                      </FormGroup>
+                    </div>
+                    <div className="mt-4">
+                      <button
+                        type="submit"
+                        className="btn btn-block btn-primary"
+                      >
+                        Entrar
+                      </button>
+                    </div>
+                  </Form>
                 </div>
-              </section>
-            </Container>
-          </>
-        )}
+              </div>
+            </section>
+          </Container>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default ClientForm
+export default ClientForm;
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { Redirect } from 'react-router-dom'
-import * as Yup from 'yup'
-import { Container, Col } from 'reactstrap'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Redirect } from "react-router-dom";
+import * as Yup from "yup";
+import { Container, Col } from "reactstrap";
 
-
-import Spinner from '../../components/icons/Spinner'
+import Spinner from "../../components/icons/Spinner";
 
 import {
   LOCAL_KEY,
@@ -25,29 +24,28 @@ import {
   getUserType,
   getAccessTokenExpiration,
   excludeData
-} from '../../services/auth'
+} from "../../services/auth";
 
-import { Creators as UserActions } from '../../store/ducks/user'
+import { Creators as UserActions } from "../../store/ducks/user";
 
 const schema = Yup.object().shape({
-  username: Yup.string().required('Campo obrigatório'),
-  password: Yup.string()
-    .required('Campo obrigatório')
-})
+  username: Yup.string().required("Campo obrigatório"),
+  password: Yup.string().required("Campo obrigatório")
+});
 class Login extends Component {
   state = {
-    usernameInput: '',
-    passwordInput: '',
+    usernameInput: "",
+    passwordInput: "",
     refreshToken: false,
-    inputType: 'password'
-  }
+    inputType: "password"
+  };
 
   componentDidMount() {
     const {
       user: { loggedIn },
       signInSuccess,
       signOut
-    } = this.props
+    } = this.props;
 
     if (getLocalObj(LOCAL_KEY) && !loggedIn) {
       if (getToken()) {
@@ -71,34 +69,40 @@ class Login extends Component {
             isEditor: getUserType().isEditor,
             isReviser: getUserType().isReviser
           }
-        }
-        signInSuccess(data, false)
+        };
+        signInSuccess(data, false);
       } else {
-        excludeData()
-        signOut()
+        excludeData();
+        signOut();
       }
     }
   }
 
   handleLogin = event => {
-    event.preventDefault()
-    const { usernameInput, passwordInput } = this.state
-    const { signInRequest } = this.props
-    signInRequest(usernameInput, passwordInput)
-  }
+    event.preventDefault();
+    const { usernameInput, passwordInput } = this.state;
+    const { signInRequest } = this.props;
+    signInRequest(usernameInput, passwordInput);
+  };
 
   showPassword = () => {
-    this.setState({ inputType: this.state.inputType === 'password' ? 'text' : 'password' })
-  }
+    this.setState({
+      inputType: this.state.inputType === "password" ? "text" : "password"
+    });
+  };
 
   render() {
     const {
-      user: { loading, loggedIn, userType: { isClient, isEditor, isReviser } },
-    } = this.props
+      user: {
+        loading,
+        loggedIn,
+        userType: { isClient, isEditor, isReviser }
+      }
+    } = this.props;
 
-    if (loggedIn && isClient) return <Redirect from="/login" to="/client" />
-    if (loggedIn && isReviser) return <Redirect from="/login" to="/reviser" />
-    if (loggedIn && isEditor) return <Redirect from="/login" to="/editor" />
+    if (loggedIn && isClient) return <Redirect from="/login" to="/client" />;
+    if (loggedIn && isReviser) return <Redirect from="/login" to="/reviser" />;
+    if (loggedIn && isEditor) return <Redirect from="/login" to="/editor" />;
 
     return (
       <>
@@ -107,77 +111,122 @@ class Login extends Component {
             <Spinner />
           </Container>
         ) : (
-            <section>
-              <Container>
-                <div className="row align-items-center justify-content-center slice ">
-                  <Col className="4 py-6 py-md-0" md="6">
-                    <div>
-                      <div className="mb-5 text-center">
-                        <h6 className="h3 mb-1">Login</h6>
-                        <p className="text-muted mb-0">Entre com sua conta para continuar.</p>
-                      </div>
-                      <span className="clearfix">
-                      </span>
-                      <form schema={schema} onSubmit={this.handleLogin}>
-                        <div className="form-group">
-                          <label className="form-control-label">Email</label>
-                          <div className="input-group input-group-merge">
-                            <input type="email" className="form-control form-control-prepend" id="input-email" placeholder="nome@exemplo.com" onChange={e => this.setState({ usernameInput: e.target.value })} />
-                            <div className="input-group-prepend">
-                              <span className="input-group-text">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-user">
-                                  <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2">
-                                  </path>
-                                  <circle cx="12" cy="7" r="4">
-                                  </circle>
-                                </svg>
-                              </span>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="form-group mb-0">
-                          <div className="d-flex align-items-center justify-content-between">
-                            <div>
-                              <label className="form-control-label">Senha</label>
-                            </div>
-                            <div className="mb-2">
-                              <a href="#" className="small text-muted text-underline--dashed border-primary" onClick={this.showPassword}>Exibir senha</a>
-                            </div>
-                          </div>
-                          <div className="input-group input-group-merge">
-                            <input type={this.state.inputType} className="form-control form-control-prepend" id="input-password" placeholder="Password" onChange={e => this.setState({ passwordInput: e.target.value })} />
-                            <div className="input-group-prepend">
-                              <span className="input-group-text">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-key">
-                                  <path d="M21 2l-2 2m-7.61 7.61a5.5 5.5 0 1 1-7.778 7.778 5.5 5.5 0 0 1 7.777-7.777zm0 0L15.5 7.5m0 0l3 3L22 7l-3-3m-3.5 3.5L19 4">
-                                  </path>
-                                </svg>
-                              </span>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="mt-4">
-                          <button type="submit" className="btn btn-block btn-primary">Entrar</button>
-                        </div>
-                      </form>
+          <section>
+            <Container>
+              <div className="row align-items-center justify-content-center slice ">
+                <Col className="4 py-6 py-md-0" md="6">
+                  <div>
+                    <div className="mb-5 text-center">
+                      <h1 className="h3 mb-1">Login</h1>
+                      <p className="text-muted mb-0">
+                        Entre com sua conta para continuar.
+                      </p>
                     </div>
-                  </Col>
-                </div>
-              </Container>
-            </section>
-          )}
+                    <span className="clearfix"></span>
+                    <form schema={schema} onSubmit={this.handleLogin}>
+                      <div className="form-group">
+                        <label className="form-control-label">Email</label>
+                        <div className="input-group input-group-merge">
+                          <input
+                            type="email"
+                            className="form-control form-control-prepend"
+                            id="input-email"
+                            placeholder="nome@exemplo.com"
+                            onChange={e =>
+                              this.setState({ usernameInput: e.target.value })
+                            }
+                          />
+                          <div className="input-group-prepend">
+                            <span className="input-group-text">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="1em"
+                                height="1em"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="feather feather-user"
+                              >
+                                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                <circle cx="12" cy="7" r="4"></circle>
+                              </svg>
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="form-group mb-0">
+                        <div className="d-flex align-items-center justify-content-between">
+                          <div>
+                            <label className="form-control-label">Senha</label>
+                          </div>
+                          <div className="mb-2">
+                            <a
+                              href="#"
+                              className="small text-muted text-underline--dashed border-primary"
+                              onClick={this.showPassword}
+                            >
+                              Exibir senha
+                            </a>
+                          </div>
+                        </div>
+                        <div className="input-group input-group-merge">
+                          <input
+                            type={this.state.inputType}
+                            className="form-control form-control-prepend"
+                            id="input-password"
+                            placeholder="Password"
+                            onChange={e =>
+                              this.setState({ passwordInput: e.target.value })
+                            }
+                          />
+                          <div className="input-group-prepend">
+                            <span className="input-group-text">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="1em"
+                                height="1em"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="feather feather-key"
+                              >
+                                <path d="M21 2l-2 2m-7.61 7.61a5.5 5.5 0 1 1-7.778 7.778 5.5 5.5 0 0 1 7.777-7.777zm0 0L15.5 7.5m0 0l3 3L22 7l-3-3m-3.5 3.5L19 4"></path>
+                              </svg>
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="mt-4">
+                        <button
+                          type="submit"
+                          className="btn btn-block btn-primary"
+                        >
+                          Entrar
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </Col>
+              </div>
+            </Container>
+          </section>
+        )}
       </>
-    )
+    );
   }
 }
 
-const mapStateToProps = state =>
-  ({
-    user: state.user
-  })
+const mapStateToProps = state => ({
+  user: state.user
+});
 
 const mapDispatchToProps = dispatch =>
+  bindActionCreators({ ...UserActions }, dispatch);
 
-  bindActionCreators({ ...UserActions }, dispatch)
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
